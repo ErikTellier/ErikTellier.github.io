ee17b9f09dc3445ce2c43494ddfc67345ec1bfac
ErikTellier <erik.tellier.etps@gmail.com>
2024-06-18 13:32:40 +0200

    randomized bird size

--- data/js/script.js
+++ data/js/script.js
@@ -1,23 +1,31 @@
+function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     var bird = document.querySelector('.bird');
     
     function randomizeBird() {
         var viewportWidth = window.innerWidth;
         
-        var baseDuration = 10; // Durée de base pour 1000px
-        var duration = (viewportWidth / 1000) * baseDuration; // Durée proportionnelle
+        var baseDuration = 10;
+        var duration = (viewportWidth / 1000) * baseDuration;
 
         duration = Math.max(5, Math.min(20, duration));
         
         bird.style.animationDuration = `1s, ${duration}s`;
         
         var viewportHeight = window.innerHeight;
-        var randomTop = Math.random() * (viewportHeight * 0.8); // entre 0 et 80% de la hauteur du viewport
+        var randomTop = Math.random() * (viewportHeight * 0.8);
         bird.style.top = `${randomTop}px`;
+
+        var randomScale = getRandom(0.4, 1);
+        bird.style.transform = `scale(${randomScale})`;
+        bird.style.transformOrigin = 'top left';
         
-        bird.style.animationName = 'none'; // Réinitialiser l'animation
-        void bird.offsetWidth; // Forcer un reflow
-        bird.style.animationName = 'fly-cycle, move-across'; // Réappliquer les animations
+        bird.style.animationName = 'none';
+        void bird.offsetWidth;
+        bird.style.animationName = 'fly-cycle, move-across'; 
     }
 
     randomizeBird();
